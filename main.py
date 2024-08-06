ca9ea8fc69aa91ff13cea880ebbf8184af3bbb12
Chethan <63234676+itsChethan283@users.noreply.github.com>
2024-08-06 21:58:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from langchain_community.document_loaders import TextLoader 
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_cohere import CohereEmbeddings
+from langchain_chroma import Chroma
+from sentence_transformers import SentenceTransformer
+
+raw_doc = TextLoader("C:/Users/cs25/OneDrive - Capgemini/GenAI/Simple RAG/simplerag/Sample.txt")
+loaded_doc = raw_doc.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+split_docs = text_splitter.split_documents(loaded_doc)
+
+# embedding_model = CohereEmbeddings(cohere_api_key="b68vUIovBR2njcHiaUTnQTKMYcBJ8krMjKz4L00W")
+# embeddings = embedding_model.embed(split_docs)
+
+embedding_model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
+embeddings = embedding_model.encode(split_docs)
+
+print(embeddings)
